8dfa419cd4b9a6a8a414ccb170bd6057a728fb00
George Cook <thegekko07@gmail.com>
2019-12-08 18:19:02 +0000

    Add files via upload

---
+++ CS-230 A2 Coursework/Checked src code/Floor.java
@@ -0,0 +1,25 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * This class draws the floor object.
+ * @author Noah Stebbings
+ * @version 1.3
+ */
+public class Floor extends Terrain {
+	/**
+	 * The constructor for the Floor object.
+	 * @param x The x Position of the basic enemy on the map
+	 * @param y The y Position of the basic enemy on the map
+	 * @param gc
+	 * @param TILE_SIZE Sets the size of the tile
+	 */
+	public Floor(int x, int y, GraphicsContext gc, int TILE_SIZE) {
+		super(x, y, gc, TILE_SIZE);
+		isPlayerWalkable = true;
+		isEnemyWalkable = true;
+		this.image = new Image("file:Art/Floor.png");
+		this.tileType = "Floor";
+	}
+
+}
