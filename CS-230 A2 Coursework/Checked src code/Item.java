75062a82b6a517ed2ca0b9bfb4413a01096dbdea
George Cook <thegekko07@gmail.com>
2019-12-08 18:21:39 +0000

    Add files via upload

---
+++ CS-230 A2 Coursework/Checked src code/Item.java
@@ -0,0 +1,30 @@
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * A general class for all items.
+ * @author Noah Stebbings
+ * @version 1.1
+ */
+public class Item extends Object {
+
+	protected boolean pickedUp = false;
+
+	/**
+	 * The constructor for the Item Class.
+	 * @param x The x Position of the basic enemy on the map
+	 * @param y The y Position of the basic enemy on the map
+	 * @param gc
+	 * @param TILE_SIZE Sets the size of the tile
+	 */
+	public Item(int x, int y, GraphicsContext gc, int TILE_SIZE) {
+		super(x, y, gc, TILE_SIZE);
+		isPlayerWalkable = true;
+		isEnemyWalkable = false;
+	}
+	
+	/**
+	 * This method is empty as it is overwritten in its subclass.
+	 */
+	@Override
+	public void interact() { }
+}
