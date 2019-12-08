5048d0db68afa19f88da97be8b5376181ee75923
George Cook <thegekko07@gmail.com>
2019-12-08 20:20:08 +0000

    Add files via upload

---
+++ CS-230 A2 Coursework/Checked src code/Useable.java
@@ -0,0 +1,31 @@
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * A general class for player interactable tiles.
+ * @author Noah Stebbings
+ * @version 1.0
+ *
+ */
+public class Useable extends Object {
+
+	/**
+	 * The constructor for this class.
+	 * @param x The x position that the object needs to be created at.
+	 * @param y The x position that the object needs to be created at.
+	 * @param gc
+	 * @param TILE_SIZE Sets the size of the tile.
+	 */
+	public Useable(int x, int y, GraphicsContext gc, int TILE_SIZE) {
+		super(x, y, gc, TILE_SIZE);
+		isPlayerWalkable = true;
+		isEnemyWalkable = false;
+	}
+	
+	/**
+	 * This method is overwritten in its subclasses.
+	 */
+	@Override
+	public void interact() {
+
+	}
+}
