cff17f968a5960df2e1716ae8f8c4acfd5c40eb9
George Cook <thegekko07@gmail.com>
2019-12-08 20:11:16 +0000

    Add files via upload

---
+++ CS-230 A2 Coursework/Checked src code/TokenDoor.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * This class draws the key door object.
+ * @author Noah Stebbings
+ * @version 1.3
+ */
+public class TokenDoor extends Door {
+
+	protected int requirements;
+
+	private ArrayList<Object> objectList;
+
+	/**
+	 * The constructor for the Token Door object.
+	 * @param x The x Position of the Token Door on the map
+	 * @param y The y Position of the Token Door on the map
+	 * @param gc
+	 * @param TILE_SIZE Sets the size of the tile
+	 * @param requirements The number of Tokens that it takes to open the door.
+	 */
+	public TokenDoor(int x, int y, GraphicsContext gc, int TILE_SIZE, int requirements) {
+		super(x, y, gc, TILE_SIZE);
+		this.requirements = requirements;
+		this.objectList = TrainCanvas.getObjects();
+		this.image = new Image("file:Art/TokenDoor.png");
+		this.tileType = "TokenDoor " + this.requirements;
+	}
+
+	/**
+	 * This returns the number of tokens the door requires to opens.
+	 * @return int
+	 */
+	@Override
+	public int getRequirments() {
+		return this.requirements;
+	}
+	
+	
+	/**
+	 * This method makes the object look like a floor tile 
+	 * when it has been interacted.
+	 */
+	@Override
+	public void interact() {
+		if (!pickedUp) {
+			if (TrainCanvas.getPlayer().getTokens() >= this.requirements) {
+				objectList.add(new Floor(this.x, this.y, gc, TILE_SIZE));
+				Sound.getSound("DoorOpen");
+				this.pickedUp = true;
+				this.isPlayerWalkable = true;
+			}
+		}
+	}
+}
